4f398f85ee5c30cfe8a2bda9875497ceba2385bf
Dannyngve5 <deparragz@gmail.com>
2023-10-15 14:01:40 -0500

    Solo se cambió unos nombres xd

--- Codigo_fuente/Main.gd
+++ Codigo_fuente/Main.gd
@@ -4,6 +4,10 @@ onready var bullet_manager = $BulletManager
 onready var player: Player = $Player
 
 var enemy = preload("res://Actors/Enemy.tscn")
+var enemy2 = preload("res://Actors/Enemy2.tscn")
+
+var array
+
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
@@ -15,9 +19,14 @@ func _ready():
 
 func _on_SpawnTimer_timeout():
 	var enemy_instance = enemy.instance()
+	var enemy_intance2= enemy2.instance()
 	add_child(enemy_instance) 
+	
+	
 	var spawn_locations = get_tree().get_nodes_in_group("spawn") 
 	var chosen_location = spawn_locations[randi() % spawn_locations.size() ]
+	
+	
 	enemy_instance.position = chosen_location.position # Establece la posición del enemigo recién generado en la ubicación elegida
 	$SpawnTimer.start() # Reinicia el temporizador para la próxima generación
 	
