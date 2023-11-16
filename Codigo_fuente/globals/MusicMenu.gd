7d13fd2324e92221b0125762658d802cf9434329
dannyngve5 <deparragz@gmail.com>
2023-11-16 08:25:38 -0500

    Corregido loop musica de menu

--- Codigo_fuente/globals/MusicMenu.gd
+++ Codigo_fuente/globals/MusicMenu.gd
@@ -1,32 +1,27 @@
 extends Node
 
 var music = preload("res://Sounds/Music/final_bosscutscene.wav")
-
+var music_stream = AudioStreamPlayer.new()
 var is_playing = false
-var music_stream
+
+func _ready():
+	music_stream.volume_db = -20
+	add_child(music_stream)
+
+	if not music_stream.is_connected("finished", self, "_on_music_finished"):
+		music_stream.connect("finished", self, "_on_music_finished")
 
 func play_music():
-	music_stream = AudioStreamPlayer.new()
-	music_stream.stream = music
-	music_stream.volume_db = -20
+	music_stream.stop()
+	music_stream.set_stream(music)
 	music_stream.play()
-	add_child(music_stream)
 	is_playing = true
-	
+
 func stop_music():
-	if is_playing and music_stream:
-		var duration = 1.0  # Duración de la transición en segundos
-		var start_volume = music_stream.volume_db
-		var end_volume = -80  # Volumen al que se detendrá la música, por ejemplo -80 para silenciarla
-		var current_time = 0.0
+	music_stream.stop()
+	is_playing = false
 
-		while current_time < duration:
-			var new_volume = lerp(start_volume, end_volume, current_time / duration)
-			music_stream.volume_db = new_volume
-			yield(get_tree().create_timer(get_process_delta_time()), "timeout")
-			current_time += get_process_delta_time()
-
-		music_stream.stop()
-		is_playing = false
-
+func _on_music_finished():
+	if is_playing:
+		play_music()
 
